90b80db7244bcf989e5be81db03a87f5f34741f3
acer <carmenjssu@gmail.com>
2025-08-01 15:55:04 +0100

    create fact sql models

--- netflix/models/staging/src_ratings.sql
+++ netflix/models/staging/src_ratings.sql
@@ -5,5 +5,6 @@ WITH raw_ratings AS (
 SELECT
     userId AS user_id,
     movieId AS movie_id,
-    TO_TIMESTAMP_LTZ(timestamp) AS rating_timestmap
+    rating,
+    TO_TIMESTAMP_LTZ(timestamp) AS rating_timestamp
 FROM raw_ratings
